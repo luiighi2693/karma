b9313216ff76c143ad76614cb37a8a70f7d47b28
luiighi2693 <luiscarvajal2693@gmail.com>
2016-11-29 22:57:03 -0400

    six commit

---
+++ gridtest/index.php
@@ -0,0 +1,196 @@
+<!doctype=HTML>
+<html>
+
+    <head>
+    <title>Karma the Game of Destiny</title>
+    
+    <meta name="description" content="Design Engineer">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript, Design, Engineering, CAD, Drafting, Virtual Reality, Game Development, App Development, Innovation, Invention, Software, Energy, Renewable, Patient Navigation, Business Navigator, Karma - the Game of Destiny, Alquimist eBook Reader, Business Navigator, Alquimi, Rene Reyes">
+    <meta name="author" content="Rene Reyes">
+    <meta charset="UTF-8">
+		
+		<link href="css/styles.css" rel="stylesheet" type="text/css" media="screen"> 
+    
+    </head>
+    
+    <body>
+    
+    		<!-- Top of Page - Main Title -->
+		
+
+				
+			
+		
+		
+			
+		<!-- Headline of Page - Titles of sections and lines -->
+
+		
+        
+        <style>
+		body 
+		{
+		background-image:url('images/background6.png');
+		background-color:#e6e6e6;
+		background-position:top center; 
+		background-size:100%;
+		background-attachment:fixed;
+		background-border:0px 0px 0px 0px;
+		}
+		</style>
+		
+		<div id="dashboard_box">
+		
+			<div id="top_box">
+				<div id="title_box">
+					<div id="title_left_box">
+					
+						<h1></h1>
+								
+					</div>	<!-- end #title_left_box -->
+					
+					<div id="title_middle_box">
+						<div id="guru_center"><a href="myfile.htm"><img src="images/guru_logo.png"></a> </div>
+					</div>	<!-- end #title_middle_box -->
+
+					<div id="title_right_box">
+					
+					
+					</div>	<!-- end #title_right_box -->
+				
+				</div>	<!-- end #title_box -->
+				
+			<div id="title_line_box"></div>	<!-- end #title_line_box -->
+
+				<div id="subtitle_box">	
+					<div id="subtitle_left_box">
+					
+						<h1></h1>
+				
+					</div>	<!-- end #subtitle_left_box -->
+					
+					
+					<div id="subtitle_right_box">
+					
+						<h1></h1>
+					
+					</div>	<!-- end #subtitle_right_box -->
+				
+				</div>	<!-- end #subtitle_box -->
+				
+			<div id="subtitle_line_box"></div>	<!-- end #subtitle_line_box -->
+
+			
+			
+			</div>	<!-- end #top_box -->
+			
+			
+				
+			
+			
+			
+			<div id="mid_box">
+			
+				<div id="left_buttons">	
+				  <div id="left_menu">  
+					<div id="box_hide_icon"><a href="myfile.htm"><img src="images/icon_hide_white.png"></a> </div>
+					<div id="box_bomb_icon"><a href="myfile.htm"><img src="images/icon_bomb_white.png"></a> </div>
+					<div id="box_stars_icon"><a href="myfile.htm"><img src="images/icon_stars_white.png"></a> </div>
+					<div id="box_challenge_icon"><a href="myfile.htm"><img src="images/icon_challenge_white.png"></a> </div>
+					<div id="box_music_icon"><a href="myfile.htm"><img src="images/icon_music_white.png"></a> </div>
+					<div id="box_zap_icon"><a href="myfile.htm"><img src="images/icon_zap_white.png"></a> </div>
+					<div id="box_bail_icon"><a href="myfile.htm"><img src="images/icon_bail_white.png"></a> </div>
+					
+				  </div>	<!-- end #left_menu -->		
+				</div>   <!-- end #left_buttons -->
+			
+			
+				<div id="popup_box">
+						<div id="grid_box">
+						
+						
+													
+						</div> <!-- end #grid_box -->
+						
+						<div id="detail_box">
+							
+						</div>	<!-- end #detail_box -->
+				</div>	<!-- end #popup_box -->		
+						
+						
+				
+				<div id="right_buttons">	
+				  <div id="right_menu">  
+					<div id="box_intro_icon"><a href="myfile.htm"><img src="images/icon_intro_white.png"></a> </div>
+					<div id="box_chat_icon"><a href="myfile.htm"><img src="images/icon_chat_white.png"></a> </div>
+					<div id="box_group_icon"><a href="myfile.htm"><img src="images/icon_group_white.png"></a> </div>
+					<div id="box_outing_icon"><a href="myfile.htm"><img src="images/outingWhiteFill.png"></a> </div>
+					<div id="box_safe_icon"><a href="myfile.htm"><img src="images/safeWhiteFill.png"></a> </div>
+					<div id="box_calendar_icon"><a href="myfile.htm"><img src="images/icon_calendar_white.png"></a> </div>
+					<div id="box_myjourneybook_icon"><a href="myfile.htm"><img src="images/icon_journeybook_white.png"></a> </div>
+					
+				  </div>	<!-- end #right_menu -->		
+				</div>   <!-- end #right_buttons -->
+				
+				
+			</div>	<!-- end #mid_box -->
+					
+			<div id="bottom_box">
+			
+				<div id="bottom_menu">  
+			
+					<div id="box_gears_icon"><a href="myfile.htm"><img src="images/icon_gears_white.png"></a> </div>
+					<div id="box_infinity_icon"><a href="myfile.htm"><img src="images/icon_infinity_white.png"></a> </div>
+					<div id="box_like_icon"><a href="myfile.htm"><img src="images/icon_like_white.png"></a> </div>
+					<div id="box_heart_icon"><a href="myfile.htm"><img src="images/icon_heart_white.png"></a> </div>
+					<div id="box_idea_icon"><a href="myfile.htm"><img src="images/icon_idea_white.png"></a> </div>
+					<div id="box_search_icon"><a href="myfile.htm"><img src="images/icon_search_white.png"></a> </div>
+					
+					<div id="box_search_field"><a href="myfile.htm"><img src="images/search_field_wh_grey.png"></a> </div>
+					 
+					<div id="box_email_icon"><a href="myfile.htm"><img src="images/icon_email_white.png"></a> </div>
+					<div id="box_bucketlist_icon"><a href="myfile.htm"><img src="images/icon_bucketlist_white.png"></a> </div>
+					<div id="box_journeybook_icon"><a href="myfile.htm"><img src="images/icon_journeybook_white.png"></a> </div>
+					<div id="box_stats_icon"><a href="myfile.htm"><img src="images/icon_stats_white.png"></a> </div>
+					<div id="box_avatar_icon"><a href="myfile.htm"><img src="images/icon_avatar_white.png"></a> </div>
+										
+				  </div>	<!-- end #bottom_menu -->
+				
+			</div>	<!-- end #bottom_box -->	
+			
+		
+		
+		
+			
+        
+			
+		 
+                    
+		</div>  <!-- end #dashboard_box -->
+		
+		
+        
+		
+		<?php
+		$IP = $_SERVER["REMOTE_ADDR"];
+		$file = "iplog.txt";
+		$txtfile = fopen($file, "a");
+		$stamp = date('Y-m-d H:i:s');
+		$page = "about.php";
+		fwrite($txtfile, "\r\n");
+		fwrite($txtfile, $IP);
+		fwrite($txtfile, "\t");
+		fwrite($txtfile, $stamp);
+		fwrite($txtfile, "\t");
+		fwrite($txtfile, $page);
+		
+		fclose($txtfile);
+		?>
+        
+
+		
+    
+    
+    </body>
+
+</html>
